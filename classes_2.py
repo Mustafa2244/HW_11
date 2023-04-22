80f411be0451afb5eb737b47028352239f4db136
Anatolii Mustafa <85567307+Mustafa2244@users.noreply.github.com>
2023-04-22 12:00:02 +0300

    Update classes_2.py

--- classes_2.py
+++ classes_2.py
@@ -100,3 +100,94 @@ class AddressBook(UserDict):
     def iterator(self, n):
         for i in range(0, len(self.data.keys()), n):
             yield [{key: value} for key, value in zip(list(self.data.keys())[i:i+n], list(self.data.values())[i:i+n])]
+
+
+def input_error(func):
+    def decorator_with_arguments(command=""):
+        try:
+            res = func(command)
+            if res is None:
+                return "Phone not found"
+        except KeyError:
+            return "KeyError"
+        except ValueError:
+            return "Phone not number"
+        except IndexError:
+            return "Give me name and phone please"
+        return res
+    return decorator_with_arguments
+
+
+telephone_book = AddressBook()
+
+
+def hello():
+    return "How can I help you?"
+
+
+@input_error
+def add(command):
+    name = command.split(" ")[1]
+    phone_number = command.split(" ")[2]
+    if len(command.split(" ")) == 4:
+        day_to_birthday = command.split(" ")[3]
+        telephone_book.add_record(Record(Name(name), [Phone(phone_number)], Birthday(day_to_birthday)))
+    else:
+        telephone_book.add_record(Record(Name(name), [Phone(phone_number)]))
+    return "Phone was successfully added"
+
+
+@input_error
+def change(command):
+    for key in telephone_book.keys():
+        if key == command.split(" ")[1]:
+            telephone_book[key].update_phone(telephone_book[key].phones[0].value, command.split(" ")[2])
+    return "Phone was successfully changed"
+
+
+@input_error
+def phone(command):
+    for user in telephone_book.keys():
+        if user == command.split(" ")[1]:
+            return ", ".join([ph.value for ph in telephone_book[user].phones])
+
+
+@input_error
+def days_to_birthday(command):
+    for user in telephone_book:
+        if user == command.split(" ")[3]:
+            return telephone_book[user].days_to_birthday()
+
+
+def show_all():
+    result = []
+    for i, users in enumerate(telephone_book.iterator(10)):
+        result.append(f"page {i + 1}:")
+        for user in users:
+            for key in user:
+                result.append(f"{key}: {', '.join([str(ph.value) for ph in telephone_book[key].phones])}")
+    return "\n".join(result)
+
+
+def main():
+    while True:
+        command = input().lower()
+        if command == "hello":
+            print(hello())
+        elif command.split(" ")[0] == "add":
+            print(add(command))
+        elif command.split(" ")[0] == "change":
+            print(change(command))
+        elif command.split(" ")[0] == "phone":
+            print(phone(command))
+        elif command == "show all":
+            print(show_all())
+        elif "days to birthday" in command:
+            print(days_to_birthday(command))
+        elif command == "exit" or command == "close" or command == "good bye" or command == ".":
+            print("Good bye!")
+            return
+
+
+if __name__ == '__main__':
+    main()
